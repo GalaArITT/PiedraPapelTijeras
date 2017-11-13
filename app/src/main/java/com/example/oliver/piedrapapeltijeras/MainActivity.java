dfd5987e9c10e1f053273abbe84497aae546ef79
GalaArITT <oljagalavizar@ittepic.edu.mx>
2017-11-12 18:56:43 -0700

    Imagenes Aleatorias

--- app/src/main/java/com/example/oliver/piedrapapeltijeras/MainActivity.java
+++ app/src/main/java/com/example/oliver/piedrapapeltijeras/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.oliver.piedrapapeltijeras;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,17 +29,28 @@ public class MainActivity extends AppCompatActivity {
         imageView.setVisibility(View.INVISIBLE);
         imageView.setBackgroundResource(R.drawable.spin_animation);
 
+
         piedras_papel_tijeras = (AnimationDrawable) imageView.getBackground();
         piedras_papel_tijeras.setOneShot(true);
     }
 
+
     public void onStartBtnClick(View v) {
+        int img = (int) (Math.random() * 3);
         imageView.setVisibility(View.VISIBLE);
         if (piedras_papel_tijeras.isRunning()) {
             piedras_papel_tijeras.stop();
         }
         piedras_papel_tijeras.start();
+        imageView.setVisibility(View.VISIBLE);
+        piedras_papel_tijeras.start();
+        Log.d("Variable img: ",""+img);
+        if(img == 0){piedras_papel_tijeras.addFrame(getResources().getDrawable(R.drawable.piedra),200);};
+        if(img == 1){piedras_papel_tijeras.addFrame(getResources().getDrawable(R.drawable.papel),200);};
+        if(img == 2){piedras_papel_tijeras.addFrame(getResources().getDrawable(R.drawable.tijeras),200);};
     }
+
+
 
     public void onStopBtnClick(View v) {
         piedras_papel_tijeras.stop();
